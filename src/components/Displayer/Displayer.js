b1434dbbc1502b1f8a55ae10d18b1d2d6190d08e
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-26 03:08:44 +0100

    displayer done

---
+++ src/components/Displayer/Displayer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Displayer.scss'
+
+function Displayer() {
+  return (
+    <div id='displayer-container'>
+      <div id='display'>
+        <div id='complete-operations'>
+          452425424 + 5152169
+        </div>
+        <div id='current-operations'>
+          452452452
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Displayer
---
+++ src/containers/Calculator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Calculator.scss'
+import Displayer from '../components/Displayer/Displayer'
+
+function Calculator() {
+  return (
+    <div id='container'>
+      <div id='calculator-background'>
+        <Displayer />
+        <div>defe</div>
+        <div>fedfeds</div>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
