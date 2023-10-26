91c189848433b055d50b374a190aebab3d0f19fa
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-26 04:06:06 +0100

    styling done

--- src/components/Inputbtns/Inputbtns.js
+++ src/components/Inputbtns/Inputbtns.js
@@ -7,20 +7,20 @@ function Inputbtns() {
         <table>
             <tr>
                 <td colspan="2"><button id='ac-btn' className='btns' value='AC'>AC</button></td>
-                <td><button className='btns operation-btns' value='/'>/</button></td>
-                <td><button className='btns operation-btns' value='X'>X</button></td>
+                <td><button className='btns operations-btns' value='/'>/</button></td>
+                <td><button className='btns operations-btns' value='X'>X</button></td>
             </tr>
             <tr>
                 <td><button className='btns' value='1'>1</button></td>
                 <td><button className='btns' value='2'>2</button></td>
                 <td><button className='btns' value='3'>3</button></td>
-                <td><button className='btns operation-btns' value='-'>-</button></td>
+                <td><button className='btns operations-btns' value='-'>-</button></td>
             </tr>
             <tr>
                 <td><button className='btns' value='4'>4</button></td>
                 <td><button className='btns' value='5'>5</button></td>
                 <td><button className='btns' value='6'>6</button></td>
-                <td><button className='btns operation-btns' value='+'>+</button></td>
+                <td><button className='btns operations-btns' value='+'>+</button></td>
             </tr>
             <tr>
                 <td><button className='btns' value='7'>7</button></td>
