9a4b956dc12214b41ac8b14a94617c3d0a4053fb
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-26 04:10:14 +0100

    default state done

--- src/components/Displayer/Displayer.js
+++ src/components/Displayer/Displayer.js
@@ -6,10 +6,10 @@ function Displayer() {
     <div id='displayer-container'>
       <div id='display'>
         <div id='complete-operations'>
-          452425424 + 5152169
+          
         </div>
         <div id='current-operations'>
-          452452452
+          0
         </div>
       </div>
     </div>
